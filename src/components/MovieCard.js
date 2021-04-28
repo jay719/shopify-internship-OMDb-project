0bcdd40cea97323e43d58fdcde1c38dbdc155395
jay719 <javariab17@gmail.com>
2021-04-28 13:44:24 -0600

    filled in the movie card data

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,10 +5,11 @@ export default function MovieCard({movie}) {
 
     return (
         <div className="Mcard"> 
-            <h1 classname="Mtitle">{movie.title}</h1>
-            <h2 classname="mrating">{movie.rating}</h2>
-            <button classname="Nbutton">Nomnate</button>
-            <img src={movie.poster} alt={movie.title} />
+            <h1 classname="Mtitle" >{movie.title}</h1>
+            <h2 classname="Mrating" >{movie.rating}</h2>
+            <p classname="Mdescription" > {movie.plot}</p>
+            <button classname="Nbutton" >Nomnate?</button>
+            <img classname="Mposter" src={movie.poster} alt={movie.title} />
         </div>
     )
 }
