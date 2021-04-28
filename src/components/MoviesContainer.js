d54b0e93d481a166d630cea728b5871179051f0a
jay719 <javariab17@gmail.com>
2021-04-28 13:53:34 -0600

    changed movies container class name and added box shadow to container

--- src/components/MoviesContainer.js
+++ src/components/MoviesContainer.js
@@ -11,7 +11,7 @@ export default function MoviesContainer() {
         
     }
     return (
-        <div classsName="Mcontainers">
+        <div classsName="Mcontainer">
             {showMovies}
         </div>
     )
