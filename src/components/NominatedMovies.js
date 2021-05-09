913ef3879b3d0a894303623a7e2a3a026d8c9edc
jay719 <javariab17@gmail.com>
2021-05-08 22:39:15 -0600

    adding restart button

--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -4,7 +4,10 @@ import React, { useState } from 'react'
 
 export default function NominatedMovies({nominations}) {
     let i = 0
-    const endMessage = "You have nominated all 5 movies!"
+    const restart = () => {
+        
+    }
+    const endMessage = `You have nominated all 5 movies!${restart}`
     const renderNominatedMovies = () => { 
             return nominations.map(nomination=> {
                 return <div>
