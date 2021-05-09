6e9e93f58dbf8f57bfb60fcdc3c946b30cb68659
jay719 <javariab17@gmail.com>
2021-05-08 22:10:48 -0600

    removed spacing in nomindated movies

--- src/components/NominatedMovies.js
+++ src/components/NominatedMovies.js
@@ -3,52 +3,33 @@ import React, { useState } from 'react'
 
 
 export default function NominatedMovies({nominations}) {
-
     let i = 0
-    const [message, setMessage] = useState(true)
     const endMessage = "You have nominated all 5 movies!"
-
     const renderNominatedMovies = () => { 
-        
             return nominations.map(nomination=> {
-        // console.log(nomination)
-
                 return <div>
                     <li key={i +1}>
                         {nomination.movie.Title} 
-                        {}
                         <p>Nominated Rating: {nomination.rating}🌟</p>
                         <p> Personal Review:{nomination.review}</p>
-                        
                     </li>
-                
                 </div>
-                
-
-                })
-            
-    
-    }
+                }
+            )
+        }
     const renderMessage = () => {
-        
         if (nominations.length < 5) {return ""}
-        else {
+            else {
             {return <p>{endMessage}</p>}
+            }
         }
-        }
-
-        
-
     return (
         <div className="nominations">
             <h1 className="Ntitle">Nominations</h1>
             {renderMessage()}
-            
             <ul className="nominated-movies">
-                
             {renderNominatedMovies()}
             </ul>
-            
         </div>
     )
 }
